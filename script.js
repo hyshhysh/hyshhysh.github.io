e95ffad12e823adebbff913595d18512524aa17e
josielim <limshjosie@gmail.com>
2025-05-03 21:09:32 +0800

    Updated design

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nav-menu');
-
-    toggle.addEventListener('click', function () {
-        nav.classList.toggle('show');
-    });
-});
